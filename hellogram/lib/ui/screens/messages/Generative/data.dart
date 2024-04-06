151002cb8f408053667237a016c2a63f545863e9
Adarsh.J <78248655+adarsh4j@users.noreply.github.com>
2024-04-06 12:23:44 +0530

    Update data.dart

--- hellogram/lib/ui/screens/messages/Generative/data.dart
+++ hellogram/lib/ui/screens/messages/Generative/data.dart
@@ -1,3 +1,3 @@
-String APIKEY = "AIzaSyCOw9YpGz1D66DlGw5iD8q3SFLrK9WOwKU";
+String APIKEY = "ADD YOUR API KEY";
 String BaseURL =
     "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=YOUR_API_KEY";
